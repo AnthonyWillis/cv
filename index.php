a556a517969bcf0891a3c153782b4a34e9a17169
Anthony Willis <anwillis80@gmail.com>
2017-09-24 19:57:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+
+		<title>Anthony Willis CV</title>
+		<meta charset="UTF-8" />
+		<link href="style/cvonline.style.css" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	</head>
+	<body>
+		<div class="cv">
+			<div class="adressewrapper">
+				<p class="adresse">
+					Anthony WILLIS.<br>
+					06 58 75 19 15.<br>
+					159 Boulevard Charles de Gaulle, <br>
+					92700 Colombes<br>
+					anwillis80@gmail.com
+				</p>
+				<a href="https://www.linkedin.com/in/anthony-willis/"><img class="lnkd" src="images/linkedinlogo.jpg" alt="linkedin logo"></a>
+			</div>
+			<img src="images/anthonywillis2.jpg" alt="Passport Photo">
+
+			<h1>DEVELOPPEUR INTEGRATEUR WEB, PHP ORIENTE OBJET</h1>
+			<h4 class="lang">Langue Maternelle - Anglais, Bilingue Français</h3>
+			<hr>
+			<div class="main">
+
+		<button class="accordion"><h2>COMPETENCES :</h2></button>
+		<div class="panel">
+
+        <button class="accordion">Informatique / Graphisme</button>
+        <div class="panel">
+          <div class="opened">
+          <h4>Informatique / Graphisme</h4>
+          <ul>
+            <li>Languages: HTML 5, CSS 3, Javascript, SQL, XML, Photoshop, PHP5, Wordpress, Bootstrap, JQuery, Symphony2</li>
+            <li>Sytèmes : Windows, Linux</li>
+			<li>Conduite d’un projet internet</li>
+			<li>Projets accomplis:
+				<ul>
+					<li><a href="http://www.willisanthony.fr/parimis2">HTML/CSS site vitrine</a></li>
+					<li><a href="http://www.willisanthony.fr/annonceo">PHP site - en travaux. HTML, CSS, PHP, MySQL. </a></li>
+				</ul>
+			</li>
+			<li>Conception d’un cahier des charges</li>
+            <li>Gestion du développement de sites</li>
+            <li>Modélisation et gestion des bases de données</li>
+			<li>Construction et gestion d'un back office</li>
+			<li>Développement d'un site web avec PHP Orienté Objet et Sympony2</li>
+		  </ul>
+          </div>
+        </div>
+			<button class="accordion">Enseignement</button>
+        <div class="panel">
+        <div class="opened">
+        <h4>Enseignement</h4>
+        <ul>
+          <li>Support pédagogique aux étudiants et collègues</li>
+          <li>Planification des cours</li>
+          <li>Direction, conseil et orientation des étudiants</li>
+          <li>Adaptation et coordination pédagogique des concours</li>
+        </ul>
+        </div>
+        </div>
+      <button class="accordion">Organisation/Problematique</button>
+        <div class="panel">
+        <div class="opened">
+        <h4>Organisation/Problematique</h4>
+        <ul>
+          <li>Analyses et adaptation aux situations diverses dans des délais très courts</li>
+          <li>Résolution des problématiques jusqu’à leurs termes</li>
+          <li>Grande attention aux détails</li>
+          <br>
+          <li>Relation publique : écoute active</li>
+          <li>Transmetteur d’information : rédacteur des présentations de powerpoint pour collègues - formation des collègues</li>
+          <br>
+
+          <li>Organisation des travaux de l’équipe des enquêteurs scientifiques</li>
+          <li>Logistique des stocks et des pièces à conviction</li>
+          <li>Rédaction de rapports de témoignages pour les tribunaux</li>
+        </ul>
+        </div>
+        </div>
+			</div>
+
+
+		<br><br>
+
+      <button class="accordion"><h2>EXPERIENCES PROFESSIONNELLES :</h2></button>
+      <div class="panel">
+
+			<button class="accordion">B2S/Nissan</button>
+      <div class="panel">
+      <div class="opened">
+      <p>
+          2015/2016 : Support technique hotline. B2S/Nissan Gennevilliers.<br>
+          Contact téléphonique, email et messagerie instantanée avec les clients FR/UK
+      </p>
+      </div>
+      </div>
+      <button class="accordion">Comptoire des Langues</button>
+      <div class="panel">
+      <div class="opened">
+      <p>
+        2014/2015 : Professeur d’Anglais. Comptoir des Langues, Paris. <br>
+        Enseignement de l’anglais à des adultes et adolescents, tous niveaux.
+      </p>
+      </div>
+      </div>
+			<button class="accordion">Metropolitan Police, London</button>
+      <div class="panel">
+      <div class="opened">
+      <p>
+        2004 – 2014 : Expert de la police scientifique. Metropolitan POLICE of LONDON.<br>
+        Etudier l’historique des pièces à conviction et des scènes de crime dans le cadre des enquêtes criminelles.
+      </p>
+      </div>
+      </div>
+			</div>
+			<br><br>
+			<button class="accordion"><h2>FORMATION :</h2></button>
+      <div class="panel">
+			<button class="accordion">IFOCOP</button>
+      <div class="panel">
+      <div class="opened">
+	  <p>
+		2017: Développeur Intégrateur Web, PHP Orienté Objet - IFOCOP Paris 11eme.
+	  </p>
+      <p>
+        2017 : Développeur Intégrateur Web – IFOCOP Montigny le Bretonneux.
+      </p>
+      </div>
+      </div>
+      <button class="accordion">Enseignement</button>
+      <div class="panel">
+      <div class="opened">
+      <p>
+        2003 : PostGraduate Certificate of Education University of NEWCASTLE <br>
+				Statut d’enseignant débutant qualifié. Expérience de l’enseignement de la biologie, de la Chimie et de la Physique au collège (âges de 11 à 16 ans).
+      </p>
+      </div>
+      </div>
+      <button class="accordion">Maîtrise de Chimie</button>
+      <div class="panel">
+      <div class="opened">
+      <p>
+        2002 : Maîtrise de Chimie MChem 2.1 University of BATH, Angleterre.<br>
+				Spécialisation en Chimie Physique et Photochimie.
+      </p>
+      </div>
+      </div>
+      </div>
+
+		<br><br>
+
+      <button class="accordion"><h2>CENTRES D'INTERET:</h2></button>
+			<div class="panel">
+
+      <button class="accordion"><p>
+        Musique, Guitare acoustique classique, flûte. Lecture Science-fiction, récits historiques, presse scientifique.
+      </p></button>
+
+      </div>
+      </div>
+      <hr>
+    <footer>
+      <h6 style="text-align: center;">Anthony Willis <?php echo date('Y');?></h6>
+	 <h6 style="text-align: center;">Hébergement : Gandi. https://www.gandi.net/</h6>
+    </footer>
+		</div>
+
+<script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+</script>
+	</body>
+</html>
